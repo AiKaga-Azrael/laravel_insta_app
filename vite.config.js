8fd61066ff24ff9b68a17e46f05c2f9230a95bcb
Ai Kaga <aikaga@AinoMacBook-Air.local>
2025-05-08 14:22:44 +0900

    Fix Vite config and build

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
+                'resources/css/app.css', // use CSS instead
                 'resources/js/app.js',
             ],
             refresh: true,
