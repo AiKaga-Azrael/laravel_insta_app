7a8f3a6cd5394eccf560a28b37f957876d81d53f
Ai Kaga <aikaga@AinoMacBook-Air.local>
2025-05-08 14:42:47 +0900

    Fix: hardcode Vite asset references for Railway

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,9 @@
     <!-- Fontawesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('build/assets/app-66QQgIxc.css') }}">
+    <script type="module" src="{{ asset('build/assets/app-D-03kJxt.js') }}"></script>
+
 
 
     <!-- Custom CSS -->
